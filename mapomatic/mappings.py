bbc98ccc8aadfba38af3019730a269db7ae3b072
Paul Nation <nonhermitian@gmail.com>
2022-02-04 04:56:41 -0500

    Add Qiskit license to header since using some of the code

--- mapomatic/mappings.py
+++ mapomatic/mappings.py
@@ -10,6 +10,22 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+# The exact_mappings routine is more or less a direct port of the VF2Layout
+# pass code from Qiskit with minor modifications to simplify and return a
+# different format.  The Qiskit code is under the following license:
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
 """Circuit manipulation tools"""
 import random
 import numpy as np
